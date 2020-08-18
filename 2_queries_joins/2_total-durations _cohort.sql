8b0d0e59948b9dad1519f319a5c24303c3f3c5d8
TimaJuma <tima.xpl@gmail.com>
2020-08-18 00:18:12 +0000

    queries using JOIN

---
+++ 2_queries_joins/2_total-durations _cohort.sql
@@ -0,0 +1,5 @@
+SELECT SUM(duration) as total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';

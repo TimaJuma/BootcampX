643777d3bfbf8ff4ea9d3e394315451a01ddf873
TimaJuma <tima.xpl@gmail.com>
2020-08-20 03:55:28 +0000

    SQL injection preventive methods implemented

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,18 @@ const pool = new Pool({
 });
 
 // get query parameters  and pass them to query
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit]
+
 const selectId = `SELECT students.id as student_id, students.name as name, cohorts.name as cohort
                   FROM students
                   JOIN cohorts ON cohorts.id = cohort_id
-                  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-                  LIMIT ${process.argv[3] || 5};`
+                  WHERE cohorts.name LIKE $1
+                  LIMIT $2;`;
                   
-pool.query(selectId)
+pool.query(selectId, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an ID : ${user.student_id} & was in the cohort :${user.cohort}`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,16 +9,21 @@ const pool = new Pool({
 });
 
 // get query parameters  and pass them to query
-const selectId = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-                  FROM teachers
-                  JOIN assistance_requests ON teacher_id = teachers.id
-                  JOIN students ON student_id = students.id
-                  JOIN cohorts ON cohort_id = cohorts.id
-                  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-                  ORDER BY teachers.name;`
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [`${cohortName}`];
+
+
+const selectTeachers = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+                        FROM teachers
+                        JOIN assistance_requests ON teacher_id = teachers.id
+                        JOIN students ON student_id = students.id
+                        JOIN cohorts ON cohort_id = cohorts.id
+                        WHERE cohorts.name = $1
+                        ORDER BY teachers.name;`
 
                   
-pool.query(selectId)
+pool.query(selectTeachers, values)
   .then(res => {
     res.rows.forEach(t => {
       console.log(`${t.cohort} : ${t.teacher}`);
